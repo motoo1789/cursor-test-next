4aca0bd6b9eaa87d0ca75c46ed9e86577afcfb48
motoo1789 <motoo1789@gmail.com>
2025-08-31 15:47:15 +0900

    reset

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white py-8 mt-16">
-      <div className="container mx-auto px-6 text-center">
-        <p>© 2023 TechBlog. All rights reserved.</p>
-      </div>
-    </footer>
-  );
-}
